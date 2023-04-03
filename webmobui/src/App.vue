0e5325f0050f5134303be961f289ab48e29747a9
Julie Achard <julie.achard@heig-vd.ch>
2023-04-03 11:58:16 +0200

    firstCommitExo1

---
+++ webmobui/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed, ref, watchEffect } from "vue";
+const baseKelvin = ref(0);
+const celsius = computed(() => baseKelvin.value - 273.15);
+const fahrenheit = computed(() => (baseKelvin.value * 9) / 5 - 459.67);
+
+function updateBaseKelvin(event) {
+  baseKelvin.value = event.target.value;
+}
+watchEffect(() => {
+  console.log(
+    `Kelvin: ${baseKelvin.value}K, Celsius: ${celsius.value}°C, Fahrenheit: ${fahrenheit.value}°F`
+  );
+});
+</script>
+
+<template>
+  <label>Kelvin</label>
+  <input type="number" :value="baseKelvin" @input="updateBaseKelvin($event)" />
+  <label>Celsius</label>
+  <input type="number" :value="celsius" @input="watchEffect" />
+  <label>Fahrenheit</label>
+  <input type="number" :value="fahrenheit" />
+</template>
+
+<style scoped>
+* {
+  display: block;
+}
+label {
+  margin-top: 1rem;
+}
+</style>
